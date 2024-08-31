23d4c8b2010b24b32e3bab822dd254fccdf15eb6
Balamuraleekrishna <kbalamuralee@gmail.com>
2024-09-01 00:58:46 +0530

    set up project with main application, blank tests, properties file and other dependencies.

---
+++ src/main/java/com/socializr/postr/PostrApplication.java
@@ -0,0 +1,11 @@
+package com.socializr.postr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostrApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PostrApplication.class, args);
+	}
+}
---
+++ src/test/java/com/socializr/postr/PostrApplicationTests.java
@@ -0,0 +1,11 @@
+package com.socializr.postr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostrApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
